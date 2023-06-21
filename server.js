b2f7fb911557874547628e1c816f30a7a3fa0440
Steven <stevenschrimmer1@gmail.com>
2023-06-21 14:17:44 -0700

    heroku try 2)

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const database = require("./db/db")
+const database = require("./DB/db")
 
 //======================================================================
 // This sets up the APP
